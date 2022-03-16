33df0a5d847f0b9a3f37cd86a3cee3156e0e41a1
esdmr <esdmr0@gmail.com>
2022-03-16 20:46:19 +0330

    Fully shim the vscode api

--- resources/vscode.js
+++ resources/vscode.js
@@ -2,8 +2,22 @@
 const vscodeApi = globalThis.acquireVsCodeApi?.();
 
 const vscode = {
-	postMessage(arg) {
-		console.log('[post-message]', arg);
+	postMessage(message, transfer = undefined) {
+		console.log('[box-drawing]', '[postMessage]', message, transfer);
+	},
+	getState() {
+		console.log('[box-drawing]', '[getState]');
+	},
+	setState(newState) {
+		console.log('[box-drawing]', '[setState]', newState);
+	},
+	/**
+	 * @param {(data: any) => void} handler
+	 */
+	onDidReceiveMessage(handler) {
+		window.addEventListener('message', ({data}) => {
+			handler(data);
+		});
 	},
 	...vscodeApi,
 };
