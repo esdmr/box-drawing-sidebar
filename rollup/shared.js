9150e68634b6ff30b42f6a1491c227b04219a711
esdmr <esdmr0@gmail.com>
2022-03-22 17:28:41 +0430

    Add some comments to build script

--- rollup/shared.js
+++ rollup/shared.js
@@ -8,9 +8,17 @@ export const isProduction = process.env.NODE_ENV === 'production';
 const config = {
 	strictDeprecations: true,
 	plugins: [
+		// `.ts` files may import other `.ts` files via a `.js` extension. It
+		// should resolve them.
 		nodeResolve({
+			// `node-resolve` does not operate on `.ts` files by default.
 			extensions: ['.ts', '.mjs', '.js', '.json', '.node'],
+			// Node.JS builtins are not available on VSCode Web.
+			preferBuiltins: false,
+			// We do not use the `commonjs` plugin (yet).
+			modulesOnly: true,
 		}),
+		// It should format/minify.
 		esbuild({
 			minify: isProduction,
 			target: 'es2020',
