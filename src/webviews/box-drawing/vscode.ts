683d66b3ecd20001cff3b077c0dbdbf0c34cb606
esdmr <esdmr0@gmail.com>
2022-04-13 21:26:34 +0430

    Remove fallback for vscode api

--- src/webviews/box-drawing/vscode.ts
+++ src/webviews/box-drawing/vscode.ts
@@ -1,35 +1,19 @@
-import parentLogger from './logger.js';
-
-const logger = parentLogger.nest('vscodeApiShim');
-
-interface VsCodeApi {
-	postMessage(message: unknown, transfer?: unknown): void;
-	getState(): void;
-	setState(newState: unknown): void;
-	onDidReceiveMessage(handler: (data: any) => void): void;
-}
-
 declare global {
 	// eslint-disable-next-line no-var
-	var acquireVsCodeApi: (() => Partial<VsCodeApi>) | undefined;
+	var acquireVsCodeApi: () => {
+		postMessage(message: unknown, transfer?: unknown): void;
+		getState(): void;
+		setState(newState: unknown): void;
+	};
 }
 
-const vscode: VsCodeApi = {
-	postMessage(message: unknown, transfer: unknown = undefined) {
-		logger.log('postMessage', message, transfer);
-	},
-	getState() {
-		logger.log('getState');
-	},
-	setState(newState: unknown) {
-		logger.log('setState', newState);
-	},
+const vscode = {
+	...globalThis.acquireVsCodeApi(),
 	onDidReceiveMessage(handler: (data: any) => void) {
 		window.addEventListener('message', ({data}) => {
 			handler(data);
 		});
 	},
-	...globalThis.acquireVsCodeApi?.(),
 };
 
 export default vscode;
